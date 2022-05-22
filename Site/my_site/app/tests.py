ec207f63766cbd0f254dc98f883fdf34e0dc0960
MrDrovE <98401317+MrDrovE@users.noreply.github.com>
2022-05-22 19:20:18 +0400

    delglobal

--- Site/my_site/app/tests.py
+++
@@ -1,6 +0,0 @@
-from django.test import TestCase
-
-import time as t
-
-print(t.time())
-# Create your tests here.
--- Site/my_site/registration/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class User(models.Model):
-    name = models.CharField(max_length=50)
-    mail = models.EmailField(max_length=75)
-    date_join = models.DateTimeField('Дата регистрации',blank=True)
-
-# Create your models here.
